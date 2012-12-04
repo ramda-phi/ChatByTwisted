223ba2e50f36f0fe54d1695e47998fb31a74f611
yago <kennedy.icu@gmail.com>
2012-12-04 21:37:07 +0900

    2012/12/4 21:37

--- chatDemo.py
+++ chatDemo.py
@@ -8,13 +8,25 @@ from twisted.python.log import ILogObserver, FileLogObserver
 from twisted.python.logfile import DailyLogFile
 
 
+class ChatRoomElement(Element):
+    loader = XMLFile(FilePath('chatroom.xml'))
+
+    @renderer
+    def content(self, request, tag):
+        return tag("")
+
+
 class ChatRoom(Resource):
     def __init__(self, roomName):
         Resource.__init__(self)
         self.roomName = roomName
 
-    def render(self, request):
-        return "ChatRoom"
+    def render_GET(self, request):
+        request.write("<!DOCTYPE html>\n")
+        flattenString(request, RoomListElement()).addCallback(
+                request.write)
+        request.finish()
+        return NOT_DONE_YET
 
 
 class ChatRoot(Resource):
@@ -32,16 +44,28 @@ class ChatRoot(Resource):
             return ChatRoom(path)
 
 
+class RoomListElement(Element):
+    loader = XMLFile(FilePath('roomlist.xml'))
+
+    @renderer
+    def content(self, request, tag):
+        return tag("")
+
+
 class ChatIndexPage(Resource):
     def __init__(self, requestedRoomList):
         Resource.__init__(self)
         self.requestedRooms = requestedRoomList
 
-    def render(self, request):
-        return "ChatIndexPage"
+    def render_GET(self, request):
+        request.write("<!DOCTYPE html>\n")
+        flattenString(request, RoomListElement()).addCallback(
+                request.write)
+        request.finish()
+        return NOT_DONE_YET
 
 
-class EnterRoom(Element):
+class EntranceElement(Element):
     loader = XMLFile(FilePath('entrance.xml'))
 
     @renderer
@@ -54,7 +78,7 @@ class Root(Resource):
 
     def render_GET(self, request):
         request.write("<!DOCTYPE html>\n")
-        flattenString(request, EnterRoom()).addCallback(
+        flattenString(request, EntranceElement()).addCallback(
                 request.write)
         request.finish()
         return NOT_DONE_YET
@@ -67,7 +91,6 @@ class Root(Resource):
 #service.setServiceParent(application)
 
 root = Resource()
-#root = Root()
 root.putChild('', Root())
 root.putChild('RoomList', ChatRoot())
 site = Site(root)
